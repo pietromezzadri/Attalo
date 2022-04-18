757a9f01f133819713690f047102472f2c8ad7c6
Pietro Nardelli Mezzadri <pietromesadri@gmail.com>
2022-04-18 20:52:06 -0300

    Improved camera system and specular light

--- src/input/input.h
+++ src/input/input.h
@@ -1,28 +1,53 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/constants.hpp>
-#include <iostream>
 
-#include "../camera/camera.h"
 #include "mouse.h"
 
 class Input
 {
 public:
-	Input();
-	~Input();
-
-public:
 	Mouse mouse;
 
 public:
-	void processInput(GLFWwindow* window, Camera* camera);
-	void processMouse(GLFWwindow* window, Camera* camera);
-	void processScroll(Camera* camera, double xoffset, double yoffset);
+	template <typename T>
+	void process_keyboard(GLFWwindow *window, T *object, float deltaTime)
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			object->ProcessKeyboard(FORWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			object->ProcessKeyboard(BACKWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			object->ProcessKeyboard(LEFT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			object->ProcessKeyboard(RIGHT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+			object->ProcessKeyboard(UP, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+			object->ProcessKeyboard(DOWN, deltaTime);
+	}
+
+
+	// glfw: whenever the mouse moves, this callback is called
+	// -------------------------------------------------------
+	template <typename T>
+	void process_mouse(GLFWwindow* window, T *object)
+	{
+		glfwGetCursorPos(window, &mouse.x, &mouse.y);
+
+		mouse.xOffset = mouse.x - mouse.lastX;
+		mouse.yOffset = mouse.lastY - mouse.y;
+
+		mouse.lastX = mouse.x;
+		mouse.lastY = mouse.y;
+
+
+		object->ProcessMouseMovement(mouse.xOffset, mouse.yOffset);
+	}
+	
 
 private:
 	void performAction(const char* state, const char* input);
--- src/shaders/color_shader.frag
+++ src/shaders/color_shader.frag
@@ -4,9 +4,12 @@ out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos;
   
-uniform vec3 lightPos; 
+uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 viewPos;
+
+vec3 calculateDiff(vec3 light_pos, vec3 light_color, vec3 frag_pos, vec3 normal); 
 
 void main()
 {
@@ -17,9 +20,26 @@ void main()
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-            
-    vec3 result = (ambient + diffuse) * objectColor;
+
+    vec3 diffuse = calculateDiff(lightPos, lightColor, FragPos, norm);
+
+    // specular
+    float specularStrength = 0.5;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+
+    vec3 result = objectColor * (ambient + diffuse + specular);
+
     FragColor = vec4(result, 1.0);
-} 
+}
+
+vec3 calculateDiff(vec3 light_pos, vec3 light_color, vec3 frag_pos, vec3 normal) {
+    vec3 lightDir = normalize(light_pos - frag_pos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = diff * light_color;
+    return diffuse;
+}
